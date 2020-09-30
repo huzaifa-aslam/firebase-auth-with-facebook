b2e2d456b92c1906e95d6696ad37dbd69bf6ebad
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-09-30 16:38:28 +0500

    code updatee

---
+++ app.js
@@ -0,0 +1,15 @@
+function fbLogin(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log("user",user)
+    // ...
+  }).catch(function(error) {
+
+    console.log(error.message)
+  });
+
+}
